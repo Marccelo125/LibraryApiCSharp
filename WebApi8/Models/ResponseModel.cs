8a0b57a22bf1a9e3e0d3b0c91beca3af9833d071
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-07 01:15:15 -0300

    add-response-model

---
+++ WebApi8/Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryApi.Models
+{
+    public class ResponseModel<T>
+    // O <T> diz que podemos receber dados do tipo generico,
+    // pois podemos ter tanto dados do tipo Author quanto
+    // dados do tipo Book
+    {
+        public T? Data { get; set; }
+        // T? - Dado generico e pode ser nulo (?)
+        public string Message { get; set; } = string.Empty;
+        // =string.Empty - Diz o valor padrão para a string, que é vazia "", se não passarmos nada
+        public bool Status { get; set; } = true;
+
+    }
+}
