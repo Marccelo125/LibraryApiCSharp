02b7f6cb03a3d6d0ff46f1f90fec16394bc60191
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-07 01:15:00 -0300

    author-model

---
+++ WebApi8/Models/AuthorModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LibraryApi.Models
+{
+    public class AuthorModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [JsonIgnore]
+        // Basicamente deixando A ICollection como opcional na criação de uma Author
+        public ICollection<BookModel> Books { get; set; }
+        // Uma lista de Objetos do tipo BookModel, chamado de Books
+    }
+}
