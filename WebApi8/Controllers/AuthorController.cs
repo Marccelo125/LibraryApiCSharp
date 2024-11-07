2a0908119e741ff83e70c9e64df2b0e7ced25348
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-07 01:14:04 -0300

    author-controller

---
+++ WebApi8/Controllers/AuthorController.cs
@@ -0,0 +1,54 @@
+﻿using LibraryApi.Dto.Author;
+using LibraryApi.Models;
+using LibraryApi.Services.Author;
+using Microsoft.AspNetCore.Mvc;
+
+// Como funciona a comunicação
+// A controller se comunica com a Interface (IAuthorInterface)
+// E a interface se comunica com a Service (AuthorService)
+
+namespace LibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet("GetAuthors")]
+        [ProducesResponseType(typeof(ResponseModel<List<AuthorModel>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ResponseModel<List<AuthorModel>>>> GetAuthors([FromServices] IAuthorInterface _authorInterface)
+        // ActionResult é basicamente para retornarmos o status code
+        {
+            var authors = await _authorInterface.GetAuthors();
+            return Ok(authors);
+            // Retornamos Ok, que todos os dados foram coletados, autores
+        }
+
+        [HttpGet("GetAuthorById/{idAuthor}")]
+        [ProducesResponseType(typeof(ResponseModel<AuthorModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ResponseModel<AuthorModel>>> GetAuthorById(int idAuthor, [FromServices] IAuthorInterface _authorInterface)
+        {
+            var authors = await _authorInterface.GetAuthorById(idAuthor);
+            return Ok(authors);
+        }
+
+        [HttpGet("GetAuthorByBookId/{idBook}")]
+        [ProducesResponseType(typeof(ResponseModel<AuthorModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ResponseModel<AuthorModel>>> GetAuthorByBookId(int idBook, [FromServices] IAuthorInterface _authorInterface)
+        {
+            var author = await _authorInterface.GetAuthorByBookId(idBook);
+            return Ok(author);
+        }
+
+        [HttpPost("CreateAuthor")]
+        [ProducesResponseType(typeof(ResponseModel<List<AuthorModel>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ResponseModel<AuthorModel>>> CreateAuthor(AuthorCreateDto authorCreateDto, [FromServices] IAuthorInterface _authorInterface)
+        {
+            var authors = await _authorInterface.CreateAuthor(authorCreateDto);
+            return Ok(authors);
+        }
+    }
+}
