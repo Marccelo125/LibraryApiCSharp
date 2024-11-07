471db3d3c0f9059a983419056ce4b3435a86a7c6
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-07 01:14:18 -0300

    add-app-db-context

---
+++ WebApi8/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Aqui estamos criando as tabelas dando um set no nosso banco DB
+        // e definindo o nome delas (Authors & Books).
+
+        // Quando rodamos o comando 'add-migration CreatingDataBase'
+        // (CreatingDataBase é o nome que escolhi para a primeira migração)
+        // Ele até aqui e pega estas tabelas que nós setamos abaixo e as constroi automaticamente
+
+        // Note que o comando para criar a migration vai mostrar um arquivo com o que será criado
+        // Para efetivamente criar uma Database utilize o comando 'update-database'
+        public DbSet<AuthorModel> Authors { get; set; }
+        public DbSet<BookModel> Books { get; set; }
+
+    }
+}
