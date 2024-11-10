d4107048d248b1c101ccad16bd41930687f96962
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-10 00:42:52 -0300

    add-author-edit-dto

---
+++ WebApi8/Dto/Author/AuthorEditDto.cs
@@ -0,0 +1,9 @@
+namespace LibraryApi.Dto.Author
+{
+    public class AuthorEditDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
