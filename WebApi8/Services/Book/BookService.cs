109956b6e3486929bfd472e69907c5b8b0acf61b
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-10 20:38:21 -0300

    add-create-edit-delete-methods-in-book-service

--- WebApi8/Services/Book/BookService.cs
+++ WebApi8/Services/Book/BookService.cs
@@ -26,7 +26,6 @@ namespace LibraryApi.Services.Book
                 response.Status = false;
                 return response;
             }
-            
         }
         public async Task<ResponseModel<BookModel>> GetBookById(int idBook)
         {
@@ -81,17 +80,105 @@ namespace LibraryApi.Services.Book
                 return response;
             }
         }
-        public Task<ResponseModel<List<BookModel>>> CreateBook(BookCreateDto bookCreateDto)
+        public async Task<ResponseModel<List<BookModel>>> CreateBook(BookCreateDto bookCreateDto)
         {
-            throw new NotImplementedException();
+            ResponseModel<List<BookModel>> response = new ResponseModel<List<BookModel>>();
+            try
+            {
+                var author = await _context.Authors.FirstOrDefaultAsync(authorDb => authorDb.Id == bookCreateDto.Author.Id);
+                // Verifica se o Author que foi passado nos parametros de criação existe no banco de dados
+
+                if (author == null)
+                {
+                    response.Message = "Nenhum registro localizado";
+                    return response;
+                }
+
+                var book = new BookModel()
+                {
+                    Title = bookCreateDto.Title,
+                    Author = author,
+                };
+
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+
+                response.Data = await _context.Books.Include(author => author.Author).ToListAsync();
+                response.Message = "Livro criado com sucesso";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
         }
-        public Task<ResponseModel<List<BookModel>>> EditBook(BookEditDto bookEditDto)
+        public async Task<ResponseModel<List<BookModel>>> EditBook(BookEditDto bookEditDto)
         {
-            throw new NotImplementedException();
+            ResponseModel<List<BookModel>> response = new ResponseModel<List<BookModel>>();
+            try
+            {
+                var author = await _context.Authors.FirstOrDefaultAsync(authorDb => authorDb.Id == bookEditDto.Author.Id);
+                var book = await _context.Books.Include(author => author.Author).FirstOrDefaultAsync(bookDb => bookDb.Id == bookEditDto.Id);
+
+                if (book == null)
+                {
+                    response.Message = "Nenhum registro de livro localizado";
+                    return response;
+                }
+
+                if (author == null)
+                {
+                    response.Message = "Nenhum registro de autor localizado";
+                    return response;
+                }
+
+                book.Title = bookEditDto.Title;
+                book.Author = author;
+
+                _context.Books.Update(book);
+                await context.SaveChangesAsync();
+
+                response.Data = await _context.Books.Include(author => author.Author).ToListAsync();
+                response.Message = "Livro editado com sucesso";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
         }
-        public Task<ResponseModel<List<BookModel>>> DeleteBook(int idBook)
+        public async Task<ResponseModel<List<BookModel>>> DeleteBook(int idBook)
         {
-            throw new NotImplementedException();
+            ResponseModel<List<BookModel>> response = new ResponseModel<List<BookModel>>();
+            try
+            {
+                var book = await _context.Books.FirstOrDefaultAsync(bookDb => bookDb.Id == idBook);
+
+                if (book == null)
+                {
+                    response.Message = "Nenhum registro localizado";
+                    return response;
+                }
+
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+
+                response.Data = await _context.Books.Include(author => author.Author).ToListAsync();
+                response.Message = "Livro removido com sucesso";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
         }
     }
 }
