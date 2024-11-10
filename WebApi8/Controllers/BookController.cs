6fff54b1b8637cb627a795984a099f3e55c2625c
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-10 20:37:07 -0300

    add-create-edit-delete-methods-in-controller

--- WebApi8/Controllers/BookController.cs
+++ WebApi8/Controllers/BookController.cs
@@ -1,3 +1,4 @@
+using LibraryApi.Dto.Book;
 using LibraryApi.Models;
 using LibraryApi.Services.Book;
 using Microsoft.AspNetCore.Mvc;
@@ -18,7 +19,7 @@ namespace LibraryApi.Controllers
             return Ok(books);
             // Retornamos Ok, que todos os dados foram coletados, autores
         }
-        
+
         [HttpGet("GetBookById")]
         [ProducesResponseType(typeof(ResponseModel<BookModel>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -27,7 +28,7 @@ namespace LibraryApi.Controllers
             var book = await _bookInterface.GetBookById(id);
             return Ok(book);
         }
-        
+
         [HttpGet("GetBooksByAuthorId")]
         [ProducesResponseType(typeof(ResponseModel<List<BookModel>>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -36,5 +37,29 @@ namespace LibraryApi.Controllers
             var books = await _bookInterface.GetBooksByAuthorId(id);
             return Ok(books);
         }
+
+        [HttpPost("CreateBook")]
+        [ProducesResponseType(typeof(ResponseModel<List<BookModel>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ResponseModel<List<BookModel>>>> CreateBook(BookCreateDto _bookCreateDto, [FromServices] IBookInterface _bookInterface)
+        {
+            var book = await _bookInterface.CreateBook(_bookCreateDto);
+            return Ok(book);
+        }
+
+        [HttpPut("EditBook")]
+        public async Task<ActionResult<ResponseModel<List<AuthorModel>>>> EditBook(BookEditDto bookEditDto, [FromServices] IBookInterface _bookInterface)
+        {
+            var authors = await _bookInterface.EditBook(bookEditDto);
+            return Ok(authors);
+        }
+        
+        [HttpDelete("DeleteBook")]
+        public async Task<ActionResult<ResponseModel<List<BookModel>>>> DeleteBook(int idBook, [FromServices] IBookInterface _bookInterface)
+        {
+            var book = await _bookInterface.DeleteBook(idBook);
+            return Ok(book);
+        }
     }
+
 }
