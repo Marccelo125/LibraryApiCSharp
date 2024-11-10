f57a5d6db9d123519174c254f2c1fa5ce0d283fb
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-10 00:43:49 -0300

    add-book-interface

--- WebApi8/Services/Book/IBookInterface.cs
+++ WebApi8/Services/Book/IBookInterface.cs
@@ -1,6 +1,15 @@
-﻿namespace LibraryApi.Services.Book
+﻿using LibraryApi.Dto.Book;
+using LibraryApi.Models;
+
+namespace LibraryApi.Services.Book
 {
     public interface IBookInterface
     {
+        Task<ResponseModel<List<BookModel>>> GetBooks();
+        Task<ResponseModel<BookModel>> GetBookById(int idBook);
+        Task<ResponseModel<List<BookModel>>> GetBooksByAuthorId(int idAuthor);
+        Task<ResponseModel<List<BookModel>>> CreateBook(BookCreateDto bookCreateDto);
+        Task<ResponseModel<List<BookModel>>> EditBook(BookEditDto bookEditDto);
+        Task<ResponseModel<List<BookModel>>> DeleteBook(int idBook);
     }
 }
